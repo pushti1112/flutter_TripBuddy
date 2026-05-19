fca94486b6ef29a5bfb1a9ecd9c131ce5a914343
shreyachauhan3009 <shreyachauhan2859@gmail.com>
2026-05-19 21:17:47 +0530

    detail_screen_1

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+  final Map<String, dynamic> place;
+
+  const DetailScreen({super.key, required this.place});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Image.network(
+                place['image'],
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+
+              Positioned(
+                top: 40,
+                left: 10,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(16),
+
+            child: SizedBox(
+              width: double.infinity,
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    place['name'],
+                    style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  Text(
+                    place['city'],
+                    style: const TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Text(
+                    place['description'],
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
