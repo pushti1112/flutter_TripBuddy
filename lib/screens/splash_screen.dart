31518ec1e70452d126f858f2ed5c041be6a9de35
pushti1112 <kotadia.pushti1112@gmail.com>
2026-05-22 12:56:54 +0530

    resizing_splash_screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/data/place_holder.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -8,85 +9,137 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
   @override
   void initState() {
     super.initState();
+
     Future.delayed(const Duration(seconds: 5), () {
-      // Navigate to the next screen after the splash screen duration
+      
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const PlaceHolder()), // Replace with your next screen
+        MaterialPageRoute(
+          builder: (context) => const PlaceHolder(),
+        ),
       );
     });
   }
 
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
-      decoration: BoxDecoration(
-        
-        // gradient: LinearGradient(
-        //   colors: [const Color.fromARGB(255, 167, 205, 237), const Color.fromARGB(255, 238, 229, 240)],
-        //   begin: Alignment.topLeft,
-        //   end: Alignment.bottomRight,
-        // ),
 
-        image: DecorationImage(
-          image: AssetImage('lib/assets/Splash_bg.jpeg'),
-          fit: BoxFit.cover,
-        ),
-      ),
+    return Scaffold(
 
-      child: Column(
-        
-        children:  [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children:  [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
+      body: SafeArea(
+
+        child: Container(
+
+          width: double.infinity,
+
+          decoration: const BoxDecoration(
+
+            image: DecorationImage(
+              image: AssetImage(
+                'lib/assets/Splash_bg.jpeg',
+              ),
+
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          child: Column(
+
+            children: [
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+
+                children: const [
+
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+
+                    child: Text(
+                      'App Version: 1.0.0',
+
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              
+              const Spacer(),
+
+              
+              const CircleAvatar(
+
+                radius: 50,
+
+                backgroundImage: AssetImage(
+                  'lib/assets/TripBuddy_Logo.jpeg',
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+
                 child: Text(
-                  'App Version: 1.0.0',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  'Your Guide To Endless Journey...',
+
+                  textAlign: TextAlign.center,
+
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              
+              const Spacer(),
+
+             
+              const CircularProgressIndicator(),
+
+              const SizedBox(height: 15),
+
+              const Text(
+
+                "Loading...",
+
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const Spacer(),
+
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+
+                child: Text(
+                  "Copyright © 2026 TripBuddy. All rights reserved.",
+
+                  textAlign: TextAlign.center,
+
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ],
           ),
-      
-          SizedBox(height: 230),
-          
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage('lib/assets/TripBuddy_Logo.jpeg'),
-          ),
-          
-          SizedBox(height: 20),
-
-          Text(
-            'Your Guide To Endless Journey...',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-      
-          SizedBox(height: 310),
-      
-          CircularProgressIndicator(),
-      
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text("Loading...", style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold)),
-          ),
-      
-          SizedBox(height: 20),
-          
-          Spacer(),
-      
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text("Copyright © 2026 TripBuddy. All rights reserved.",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
-          ),
-      
-        ],
+        ),
       ),
     );
   }
-  }
+}
